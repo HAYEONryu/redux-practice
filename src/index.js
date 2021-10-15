8a470538865d9b0022eddddbb1ba4a870335ba62
HAYEONryu <21700470@handong.edu>
2021-10-15 17:04:13 +0900

    ..

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import './index.css';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
 import rootReducer from './modules';
-
-const store = createStore(rootReducer, composeWithDevTools());
-
+import {createLogger} from 'redux-logger' 
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger));
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
 
 // serviceWorker.unregister();
---
+++ src/lib/loggerMiddleware.js
@@ -0,0 +1,6 @@
+const loggerMiddleware = (store)=>{
+    return next=>{
+        return action=>{};
+    };
+};
+//미들웨어는 함수를 반환하는 함수이다. 
